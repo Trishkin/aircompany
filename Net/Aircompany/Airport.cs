3dbd72c39287bc25d9c234694912d8d085fc1df2
“Kulikov <dima.dmitry.kulikov@gmail.com>
2021-11-11 16:13:30 +0300

    restore

---
+++ Net/Aircompany/Airport.cs
@@ -0,0 +1,76 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        public List<Plane> Planes { get; set; }
+        public Airport(IEnumerable<Plane> planes)
+        {
+            Planes = planes.ToList();
+        }
+
+        public List<PassengerPlane> GetPassengersPlanes()
+        {
+            List<PassengerPlane> passengerPlanes = new List<PassengerPlane>();
+            for (int i=0; i < Planes.Count; i++)
+            {
+                if (Planes[i].GetType() == typeof(PassengerPlane))
+                {
+                    passengerPlanes.Add((PassengerPlane)Planes[i]);
+                }
+            }
+            return passengerPlanes;
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanes()
+        {
+            List<MilitaryPlane> militaryPlanes = new List<MilitaryPlane>();
+            for (int i = 0; i < Planes.Count; i++)
+            {
+                if (Planes[i].GetType() == typeof(MilitaryPlane))
+                {
+                    militaryPlanes.Add((MilitaryPlane)Planes[i]);
+                }
+            }
+            return militaryPlanes;
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            return GetPassengersPlanes().Aggregate((w, x) => w.PassengersCapacity > x.PassengersCapacity ? w : x);             
+        }
+
+        public List<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            return GetMilitaryPlanes().Where(x => x.MilitaryType == MilitaryType.Transport).ToList();
+        }
+
+        public Airport SortByMaxDistance()
+        {
+            return new Airport(Planes.OrderBy(w => w.MaxFlightDistance));
+        }
+
+        public Airport SortByMaxSpeed()
+        {
+            return new Airport(Planes.OrderBy(w => w.MaxSpeed));
+        }
+
+        public Airport SortByMaxLoadCapacity()
+        {
+            return new Airport(Planes.OrderBy(w => w.MaxLoadCapacity));
+        }
+
+
+        public override string ToString()
+        {
+            return "Airport{" +
+                    "planes=" + string.Join(", ", Planes.Select(x => x.Model)) +
+                    '}';
+        }
+    }
+}
---
+++ Net/Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,39 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        public MilitaryType MilitaryType { get; set; }
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            MilitaryType = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var plane = obj as MilitaryPlane;
+            return plane != null &&
+                   base.Equals(obj) &&
+                   MilitaryType == plane.MilitaryType;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + MilitaryType.GetHashCode();
+            return hashCode;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", MilitaryType=" + MilitaryType +
+                    '}');
+        }        
+    }
+}
